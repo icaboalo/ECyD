43b787e13964376f11509257b088d32941b8bd74
icaboalo <icaboalo@gmail.com>
2015-11-25 10:45:34 -0600

    features

--- app/src/main/java/com/icaboalo/ecyd/ui/activity/LogInActivity.java
+++ app/src/main/java/com/icaboalo/ecyd/ui/activity/LogInActivity.java
@@ -5,18 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.icaboalo.ecyd.R;
+import com.icaboalo.ecyd.util.VUtil;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LogInActivity extends AppCompatActivity {
+
+    @Bind(R.id.username_input)
+    EditText mUsernameInput;
+
+    @Bind(R.id.password_input)
+    EditText mPasswordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = ButterKnife.findById(this, R.id.app_bar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+    }
 
+    @OnClick(R.id.login) void login(){
+        if (formFilled()){
+//            Intent goToMain = new Intent(this, MainActivity.class);
+//            startActivity(goToMain);
+        }else {
+            mUsernameInput.setError(getString(R.string.username_input_error));
+            mPasswordInput.setError(getString(R.string.password_input_error));
+        }
+    }
+
+    @OnClick(R.id.register) void register(){
+//        Intent goToRegister = new Intent(this, RegisterActivity.class);
+//        startActivity(goToRegister);
     }
 
     @Override
@@ -39,5 +66,17 @@ public class LogInActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean isUsernameEmpty(){
+        return VUtil.extractEditText(mUsernameInput).isEmpty();
+    }
+
+    private boolean isPasswordEmpty(){
+        return VUtil.extractEditText(mPasswordInput).isEmpty();
+    }
+
+    private boolean formFilled(){
+        return !(isUsernameEmpty()||isPasswordEmpty());
     }
 }

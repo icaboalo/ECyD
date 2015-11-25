61c13b4e7875355ada4c34b883705c57f1f2e78e
icaboalo <icaboalo@gmail.com>
2015-11-24 20:17:51 -0600

    login design

--- app/src/main/java/com/icaboalo/ecyd/ui/activity/LogInActivity.java
+++ app/src/main/java/com/icaboalo/ecyd/ui/activity/LogInActivity.java
@@ -1,13 +1,12 @@
-package com.icaboalo.ecyd;
+package com.icaboalo.ecyd.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.icaboalo.ecyd.R;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -18,14 +17,6 @@ public class LogInActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override

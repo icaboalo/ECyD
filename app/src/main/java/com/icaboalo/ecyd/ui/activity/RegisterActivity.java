9407eea05ec0b921f3e51fd485b40ed28f894b8d
icaboalo <icaboalo@gmail.com>
2015-11-26 08:12:31 -0600

    ifs for form filled

--- app/src/main/java/com/icaboalo/ecyd/ui/activity/RegisterActivity.java
+++ app/src/main/java/com/icaboalo/ecyd/ui/activity/RegisterActivity.java
@@ -1,17 +1,28 @@
 package com.icaboalo.ecyd.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.EditText;
 
 import com.icaboalo.ecyd.R;
+import com.icaboalo.ecyd.util.VUtil;
 
+import java.util.List;
+
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @Bind(R.id.name_input)
+    EditText mNameInput;
+
+    @Bind(R.id.username_input)
+    EditText mUsernameInput;
+
+    @Bind({R.id.password_input, R.id.password_confirm_input})
+    List<EditText> mPasswordsInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +31,28 @@ public class RegisterActivity extends AppCompatActivity {
         Toolbar toolbar = ButterKnife.findById(this, R.id.app_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ButterKnife.bind(this);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    private boolean isNameEmpty(){
+        return VUtil.extractEditText(mNameInput).isEmpty();
+    }
+
+    private boolean isUsernameEmpty(){
+        return VUtil.extractEditText(mUsernameInput).isEmpty();
+    }
+
+    private boolean isPasswordEmpty(){
+        for (EditText password : mPasswordsInput){
+            if (VUtil.extractEditText(password).isEmpty()){
+                return true;
             }
-        });
+        }
+        return false;
+    }
+
+    private boolean passwordsMatch(){
+        return VUtil.extractEditText(mPasswordsInput.get(0)).contentEquals(VUtil.extractEditText(mPasswordsInput.get(1)));
     }
 
 }

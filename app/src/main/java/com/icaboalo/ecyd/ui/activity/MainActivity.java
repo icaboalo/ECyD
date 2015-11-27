d0ec72d4ece1fbd24ea5056d28b548849a19cf6b
icaboalo <icaboalo@gmail.com>
2015-11-27 09:28:52 -0600

    log out

--- app/src/main/java/com/icaboalo/ecyd/ui/activity/MainActivity.java
+++ app/src/main/java/com/icaboalo/ecyd/ui/activity/MainActivity.java
@@ -1,14 +1,14 @@
 package com.icaboalo.ecyd.ui.activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import com.icaboalo.ecyd.R;
 import com.icaboalo.ecyd.domain.TeamModel;
@@ -26,6 +26,7 @@ import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,15 +43,6 @@ public class MainActivity extends AppCompatActivity {
         isUserLogged();
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -60,7 +52,29 @@ public class MainActivity extends AppCompatActivity {
         getTeams();
     }
 
-    void getTeams(){
+    @OnClick(R.id.fab) void addTeam(){
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case (R.id.action_log_out):
+                ParseUser.logOut();
+                isUserLogged();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void getTeams() {
         String user = ParseUser.getCurrentUser().getUsername();
         ParseQuery<ParseObject> query = ParseQuery.getQuery("Teams").whereContains("user", user);
         query.findInBackground(new FindCallback<ParseObject>() {
@@ -75,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
                     mRecyclerAdapter = new TeamRecyclerAdapter(MainActivity.this, newTeamList, new ViewHolderClick() {
                         @Override
                         public void onClick(View view, int position) {
-                            Toast.makeText(MainActivity.this, newTeamList.get(position).getTeamName(), Toast.LENGTH_SHORT).show();
+                            saveOnSharedPrefeces(newTeamList.get(position).getTeamName());
                         }
                     });
                     mTeamList.setAdapter(mRecyclerAdapter);
@@ -84,16 +98,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    void setupTeamRecycler(){
+    void setupTeamRecycler() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mTeamList.setLayoutManager(linearLayoutManager);
     }
 
-    private void isUserLogged(){
+    private void isUserLogged() {
         ParseUser user = ParseUser.getCurrentUser();
-        if (user == null){
+        if (user == null) {
             VUtil.goToActivity(this, LogInActivity.class);
             finish();
         }
+    }
+
+    private void saveOnSharedPrefeces(String team){
+        SharedPreferences sharedPreferences = getSharedPreferences("team", MODE_PRIVATE);
+        sharedPreferences.edit().putString("teamName", team).apply();
     }
 }

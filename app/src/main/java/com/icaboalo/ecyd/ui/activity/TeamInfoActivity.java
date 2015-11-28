c94c6152cb8b9237d9667f764fcc4463438a0edd
icaboalo <icaboalo@gmail.com>
2015-11-27 21:44:28 -0600

    item list

--- app/src/main/java/com/icaboalo/ecyd/ui/activity/TeamInfoActivity.java
+++ app/src/main/java/com/icaboalo/ecyd/ui/activity/TeamInfoActivity.java
@@ -1,11 +1,8 @@
 package com.icaboalo.ecyd.ui.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.icaboalo.ecyd.R;
 
@@ -15,17 +12,8 @@ public class TeamInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_info);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
